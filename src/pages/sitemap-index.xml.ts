5e2e418709b92f76c96efd9cffe907eca62b9cdf
HajimiKitty <winniringy@gmail.com>
2026-08-29 12:26:58 +0800

    fix: make sitemap index reference page and image sitemaps

--- src/pages/sitemap-index.xml.ts
+++ src/pages/sitemap-index.xml.ts
@@ -1,10 +1,4 @@
 import type { APIRoute } from 'astro';
-import {
-  getArtworks,
-  getCharacters,
-  getMythologies,
-  getWorlds,
-} from '../lib/content/repositories';
 
 export const prerender = false;
 
@@ -16,77 +10,14 @@ const escapeXml = (value: string) =>
     .replaceAll('"', '&quot;')
     .replaceAll("'", '&apos;');
 
-const absolute = (path: string, site: URL) => new URL(path, site).toString();
-
-const imageXml = (src: string | undefined, title: string | undefined, site: URL) => {
-  if (!src) return '';
-  const loc = escapeXml(absolute(src, site));
-  const caption = title ? `<image:title>${escapeXml(title)}</image:title>` : '';
-  return `<image:image><image:loc>${loc}</image:loc>${caption}</image:image>`;
-};
-
-const urlXml = (loc: string, image = '') =>
-  `<url><loc>${escapeXml(loc)}</loc>${image}</url>`;
-
-export const GET: APIRoute = async ({ locals, site }) => {
+export const GET: APIRoute = ({ site }) => {
   if (!site) {
     return new Response('Astro.site is required for sitemap generation.', { status: 500 });
   }
 
-  const db = locals.runtime.env.DB;
-  const [mythologies, worlds, characters, artworks] = await Promise.all([
-    getMythologies(db),
-    getWorlds(db),
-    getCharacters(db),
-    getArtworks(db),
-  ]);
-
-  const urls: string[] = [
-    '/',
-    '/explore/',
-    '/mythology/',
-    '/world/',
-    '/character/',
-    '/wallpaper/',
-  ].map((path) => urlXml(absolute(path, site)));
-
-  for (const mythology of mythologies) {
-    urls.push(
-      urlXml(
-        absolute(`/mythology/${mythology.slug}/`, site),
-        imageXml(mythology.heroImage?.src, `${mythology.name}神话`, site),
-      ),
-    );
-  }
-
-  for (const world of worlds) {
-    urls.push(
-      urlXml(
-        absolute(`/world/${world.slug}/`, site),
-        imageXml(world.heroImage?.src, `${world.name} · ${world.nameEn}`, site),
-      ),
-    );
-  }
-
-  for (const character of characters) {
-    urls.push(
-      urlXml(
-        absolute(`/character/${character.slug}/`, site),
-        imageXml(character.portrait?.src, `${character.name} · ${character.nameEn}`, site),
-      ),
-    );
-  }
-
-  for (const artwork of artworks) {
-    urls.push(
-      urlXml(
-        absolute(`/wallpaper/${artwork.slug}/`, site),
-        imageXml(artwork.image?.src, artwork.title, site),
-      ),
-    );
-  }
-
-  const body = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">${urls.join('')}</urlset>`;
+  const pages = new URL('/sitemap-pages.xml', site).toString();
+  const images = new URL('/sitemap-images.xml', site).toString();
+  const body = `<?xml version="1.0" encoding="UTF-8"?>\n<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><sitemap><loc>${escapeXml(pages)}</loc></sitemap><sitemap><loc>${escapeXml(images)}</loc></sitemap></sitemapindex>`;
 
   return new Response(body, {
     headers: {
