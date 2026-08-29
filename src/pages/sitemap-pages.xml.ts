549f73a73d9dd2be77f707b2bc95f3a99fb188fb
HajimiKitty <winniringy@gmail.com>
2026-08-29 12:26:30 +0800

    feat: add dynamic page sitemap

---
+++ src/pages/sitemap-pages.xml.ts
@@ -0,0 +1,56 @@
+import type { APIRoute } from 'astro';
+import {
+  getArtworks,
+  getCharacters,
+  getMythologies,
+  getWorlds,
+} from '../lib/content/repositories';
+
+export const prerender = false;
+
+const escapeXml = (value: string) =>
+  value
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&apos;');
+
+const absolute = (path: string, site: URL) => new URL(path, site).toString();
+const urlXml = (loc: string) => `<url><loc>${escapeXml(loc)}</loc></url>`;
+
+export const GET: APIRoute = async ({ locals, site }) => {
+  if (!site) {
+    return new Response('Astro.site is required for sitemap generation.', { status: 500 });
+  }
+
+  const db = locals.runtime.env.DB;
+  const [mythologies, worlds, characters, artworks] = await Promise.all([
+    getMythologies(db),
+    getWorlds(db),
+    getCharacters(db),
+    getArtworks(db),
+  ]);
+
+  const urls = [
+    '/',
+    '/explore/',
+    '/mythology/',
+    '/world/',
+    '/character/',
+    '/wallpaper/',
+    ...mythologies.map((item) => `/mythology/${item.slug}/`),
+    ...worlds.map((item) => `/world/${item.slug}/`),
+    ...characters.map((item) => `/character/${item.slug}/`),
+    ...artworks.map((item) => `/wallpaper/${item.slug}/`),
+  ];
+
+  const body = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls.map((path) => urlXml(absolute(path, site))).join('')}</urlset>`;
+
+  return new Response(body, {
+    headers: {
+      'content-type': 'application/xml; charset=utf-8',
+      'cache-control': 'public, max-age=300, s-maxage=3600',
+    },
+  });
+};
