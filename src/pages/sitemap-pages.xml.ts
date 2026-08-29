9c8637127136f1c975a5b648ed72db06dc99c676
HajimiKitty <winniringy@gmail.com>
2026-08-29 12:28:56 +0800

    fix: paginate dynamic sitemap entity queries

--- src/pages/sitemap-pages.xml.ts
+++ src/pages/sitemap-pages.xml.ts
@@ -8,6 +8,8 @@ import {
 
 export const prerender = false;
 
+const PAGE_SIZE = 1000;
+
 const escapeXml = (value: string) =>
   value
     .replaceAll('&', '&amp;')
@@ -19,6 +21,15 @@ const escapeXml = (value: string) =>
 const absolute = (path: string, site: URL) => new URL(path, site).toString();
 const urlXml = (loc: string) => `<url><loc>${escapeXml(loc)}</loc></url>`;
 
+async function loadAllPaged<T>(fetchPage: (offset: number) => Promise<T[]>): Promise<T[]> {
+  const items: T[] = [];
+  for (let offset = 0; ; offset += PAGE_SIZE) {
+    const page = await fetchPage(offset);
+    items.push(...page);
+    if (page.length < PAGE_SIZE) return items;
+  }
+}
+
 export const GET: APIRoute = async ({ locals, site }) => {
   if (!site) {
     return new Response('Astro.site is required for sitemap generation.', { status: 500 });
@@ -27,9 +38,9 @@ export const GET: APIRoute = async ({ locals, site }) => {
   const db = locals.runtime.env.DB;
   const [mythologies, worlds, characters, artworks] = await Promise.all([
     getMythologies(db),
-    getWorlds(db),
+    loadAllPaged((offset) => getWorlds(db, { limit: PAGE_SIZE, offset })),
     getCharacters(db),
-    getArtworks(db),
+    loadAllPaged((offset) => getArtworks(db, { limit: PAGE_SIZE, offset })),
   ]);
 
   const urls = [
